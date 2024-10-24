db16121f8cf390cae25e4cddd27867d29f3d5ad1
HnrqHolanda <henriqueholandad@gmail.com>
2024-10-23 23:55:05 -0300

    aspas

---
+++ frontend/src/components/addmodal2.js
@@ -0,0 +1,108 @@
+import styles from '../styles/components/addmodal.module.css'
+
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Addmodal(){
+
+    const [bancoSelecionado, setBancoSelecionado] = useState('');
+
+    const bancos = [
+        "Banco do Brasil",
+        "Caixa Econômica",
+        "Bradesco",
+        "Itaú",
+        "Santander",
+        "Nubank"
+    ];
+
+    const[montante, setMontante] = useState('');
+    const[id, setID] = useState('');
+
+    const navigate = useNavigate();
+
+    //erros
+    const [erro1, setErro1] = useState(false);
+    const [erro3, seterro3] = useState(false);
+
+    const handleBancoChange = (event) => {
+        setBancoSelecionado(event.target.value);
+    };
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setErro1(false);
+        seterro3(false);
+
+
+        if(montante === ''){
+            setErro1(true);
+        } else{
+
+            try {
+                // Cria/atualiza um documento com um ID específico
+                console.log("teste")
+            
+            } 
+            
+            catch (e) {
+                console.error("Erro: ", e);
+            }
+
+        }
+         
+    };
+
+    return(
+      <div className={styles.Container}>
+        <h2>Adicionar Entrada</h2>
+        <div className={styles.forms}>
+          <div className={styles.inputs}>
+            {/* <label htmlFor="Valor">Valor em Reais: </label> */}
+            <input
+              type="Valor em Reais"
+              placeholder="Valor em Reais"
+              id="Valor em Reais"
+              value={montante}
+              onChange={(e) => setMontante(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="ID"
+              id="Id"
+              value={id}
+              onChange={(e) => setID(e.target.value)}
+            />
+          </div>
+          <select 
+                id="bancos" 
+                value={bancoSelecionado} 
+                onChange={handleBancoChange} 
+                required
+            >
+                <option value="" disabled>Escolha um banco</option>
+                {bancos.map((banco, index) => (
+                    <option key={index} value={banco}>
+                        {banco}
+                    </option>
+                ))}
+          </select>
+          {erro1 && (
+            <div className={styles.erro}>
+              <h4>Formulário não submetido. Preencha os campos vazios</h4>
+            </div>
+          )}
+          {erro3 && (
+            <div className={styles.erro}>
+              <h4>Formulário não submetido. As Nomes não coincidem!</h4>
+            </div>
+          )}
+          <button onClick={handleSubmit} style = {{width: '50%'}}>Adicionar Entrada</button>
+        </div>
+      </div>  
+      
+    )
+}
+
+export default Addmodal;
