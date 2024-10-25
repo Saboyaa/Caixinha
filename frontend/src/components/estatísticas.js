260f7212916ad101d11536fffb1f2a7838d7fd97
pedro <pedroromerorodrigues@outlook.com>
2024-10-24 21:58:39 -0300

    implementou stats

--- frontend/src/components/estatísticas.js
+++ frontend/src/components/estatísticas.js
@@ -6,29 +6,44 @@ import { useState, useEffect } from 'react';
 
 
 const Estatísticas = () => {
+    const { transactions, setTransactions } = useState([]);
 
-    const gastosFake = [
-        { valor: 800, motivo: 'Lazer' },
-        { valor: 1200, motivo: 'Educação' },
-        { valor: 350, motivo: 'Alimentação' },
-        { valor: 600, motivo: 'Transporte' },
-        { valor: 1500, motivo: 'Saúde' },
-        { valor: 450, motivo: 'Vestuário' },
-    ];
+    useEffect(() => {
+        fetch('http://localhost:8080/transactions/userId/' + token.userId, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + token.token
+            }
+        })
+        .then((res) => {
+            if (res.status !== 200) {
+                throw new Error('Falha de requisição das transações!');
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setTransactions(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
 
-    const Gastos = []
-
-    const gastosFiltrados = Gastos.filter(gasto => gasto.valor < 0)
-    
-
-    //Passar o gastos Filtrados na componente gráfico.
-
+    const transactionsFiltered = transactions.filter(gasto => gasto.transactionValue < 0);
+    let typesSums;
+    transactionsFiltered.forEach(item => {
+        if (typesSums[item.type]) {
+            typesSums[item.type] += item.transactionValue;
+        } else {
+            typesSums[item.type] = item.transactionValue;
+        }
+    });
 
     return (
         <div className={styles.container}>
             <h2>Estatísticas</h2>
             <br></br>
-            <Gráfico array={gastosFake}/>
+            <Gráfico array={typesSums}/>
         </div>
     );
 }
